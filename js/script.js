c21f1735052c0058e193e9030c7b3dbf6d471d9e
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-17 15:30:18 +0300

    fix: change styles for visual display of buttons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+const operationButtons = document.querySelectorAll("[data-operation]");
+console.log(operationButtons);
+
+
+const valueButtons = document.querySelectorAll("[data-value]");
+console.log(valueButtons);
